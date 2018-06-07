8187894a0e65edb9c89925d95af014ec7e63e099
evsyakova <katya.98.nsk@mail.ru>
2018-06-07 21:50:31 +0300

    division by zero

--- modules/arbitrary_precision_calculator/test/test_arbitrary_precision_calculator.cpp
+++ modules/arbitrary_precision_calculator/test/test_arbitrary_precision_calculator.cpp
@@ -336,7 +336,7 @@ TEST_F(ArbitraryPrecisionCalculatorTest, Cannot_Divide_By_Zero) {
     ArbitraryPrecisionCalculator z1("324546789888765434567890987654323");
 
     // Act & Assert
-    ASSERT_ANY_THROW(z1 / 0);
+    ASSERT_ANY_THROW(z1 / static_cast<ArbitraryPrecisionCalculator>(0));
 }
 
 TEST_F(ArbitraryPrecisionCalculatorTest, Number_Is_Equal_To_Itself) {
